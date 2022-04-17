876f2831bf054ab0467dd898cd95d08e58f9237c
Vincent Vic <192391297@qq.com>
2022-04-17 16:43:25 +0800

    转换antdv配置修改

--- vue.config.js
+++ vue.config.js
@@ -30,23 +30,6 @@ if (NODE_ENV === 'lib') {
   appFileName = 'Lib'
 }
 
-const mainFileStr = `
-import Vue from 'vue'
-import App from './${appFileName}.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.config.productionTip = false
-
-Vue.use(ElementUI)
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
-`
-
-fs.writeFileSync('./src/main.js', mainFileStr)
-
 const resolve = dir => {
   return path.join(__dirname, dir)
 }
